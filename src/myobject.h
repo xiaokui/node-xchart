eadec7c638b7cdba9e46f002789ddaf37ef1381f
zhao li kui <xiaokui.zhao@gmail.com>
2012-11-28 16:27:35 +0800

    ci. format code.

--- src/myobject.h
+++ src/myobject.h
@@ -5,16 +5,16 @@
 #include <node.h>
 
 class MyObject : public node::ObjectWrap {
-     public:
-           static void Init(v8::Handle<v8::Object> target);
+    public:
+        static void Init(v8::Handle<v8::Object> target);
 
-            private:
-             MyObject();
-               ~MyObject();
+    private:
+        MyObject();
+        ~MyObject();
 
-                 static v8::Handle<v8::Value> New(const v8::Arguments& args);
-                   static v8::Handle<v8::Value> PlusOne(const v8::Arguments& args);
-                     double counter_;
+        static v8::Handle<v8::Value> New(const v8::Arguments& args);
+        static v8::Handle<v8::Value> PlusOne(const v8::Arguments& args);
+        double counter_;
 };
 
 #endif
